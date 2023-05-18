3f737bbb5479924dd1b16e674fd307afdee83a08
Liquan Yang <yang.liquan87@gmail.com>
2023-05-17 22:52:15 -0400

    add constants & statics lesson

---
+++ constants_statics/src/main.rs
@@ -0,0 +1,10 @@
+const MAX_PLAYER: u8 = 10;
+static CASINO_NAME: &str = "Rusty Casino";
+
+fn main() {
+    let a = MAX_PLAYERS;
+    let b = MAX_PLAYERS;
+
+    let c = CASINO_NAME;
+    let d = CASINO_NAME;
+}
