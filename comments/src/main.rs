a9d86c5a40260515d8b72494cf202da4259fb248
Liquan Yang <yang.liquan87@gmail.com>
2023-05-23 22:44:06 -0400

    add comments

---
+++ comments/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // this is an inline comment
+    // another inline comment
+
+    /* this is a block comment
+    that spans
+    multiple lines
+    */
+
+    /* block comments
+     * can be formatted however you want
+     * such as this one that uses asterisks
+     */
+    println!("Hello, world!");
+}
