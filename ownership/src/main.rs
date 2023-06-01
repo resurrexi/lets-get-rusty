450a15d9886149f033efa2402764077b4e11d0ee
Liquan Yang <yang.liquan87@gmail.com>
2023-05-31 22:00:40 -0400

    fix comments

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     // 3. When the owner goes out of scope, the value is dropped
 
     let s1 = String::from("Rust"); // heap allocated string
-                                   // let s2 = s1;  // s2 will take ownership over s1, s1 will be destroyed
-    let s2 = s1.clone(); // need to clone to have s2 owner a copy of the value
+
+    // let s2 = s1;  // s1 transfers ownership to s2; s1 pointer is destroyed
+    let s2 = s1.clone(); // need to clone to have s2 own a copy of the value
     println!("s1 is: {s1}");
 
     let x = 10;
