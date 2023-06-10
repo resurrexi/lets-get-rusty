c06957b9883fca0f88a6400cfcf7f8fb647b488d
Liquan Yang <yang.liquan87@gmail.com>
2023-06-10 15:48:34 -0400

    add borrowing

---
+++ borrowing/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let mut s1 = String::from("Rust"); // heap allocated string
+    let r1 = &s1; // r1 references s1 (borrows from s1)
+    print_string(r1);
+    let r2 = &mut s1;
+    add_to_string(r2);
+    println!("s1 is: {s1}");
+    let s2 = generate_string();
+    println!("s2 is: {s2}");
+}
+
+fn generate_string() -> String {
+    String::from("Ferris")
+}
+
+fn add_to_string(p1: &mut String) {
+    p1.push_str(" is awesome!");
+}
+
+fn print_string(p1: &String) {
+    println!("{p1}");
+}
